372f246d4c02387b617dd30f0ba2a0b83e489029
Tuhin Nair <tuhinnair444@gmail.com>
2020-09-29 21:07:35 +0700

    init

---
+++ src/app/express/last-resort-error-handler.js
@@ -0,0 +1,8 @@
+function lastResortHandler(err, req, res, next) {
+    const traceId = req.context ? req.context.traceId : 'none';
+    console.error(traceId, err);
+
+    res.status(500).send('error');
+}
+
+module.exports = lastResortHandler;
